d5e843c14e07cbd8514eef1ab5af661e6506b815
kimakowkin <dimatosha@gmail.com>
2023-10-07 21:17:07 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Data;
+
+namespace Calculator2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Calculate())
+            {
+                Calculate();
+            }
+        }
+
+        static bool Calculate()
+        {
+            Console.WriteLine("Введите операцию: ");
+            try
+            {
+                string operation = Console.ReadLine();
+                if (string.IsNullOrEmpty(operation))
+                {
+                    throw (new Exception("Неверная операция"));
+                }
+                string value = new DataTable().Compute(operation, null).ToString();
+                Console.WriteLine(value);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return true;
+            }
+        }
+    }
+}
